6a42a46654b22276f85f183c1b2f7bbc9190cb43
Elvis Saavedra <elvis.saavedra.segura@gmail.com>
2022-03-19 00:02:15 -0500

    adding messages box

--- src/components/Chat.vue
+++ src/components/Chat.vue
@@ -3,12 +3,20 @@
     <h1>Messages</h1>
     <hr />
 
+    <div ref="chatMessage" id="chat-messages">
+      <ul class="text-start m-0 p-0">
+        <li v-for="(message, index) of messages" :key="index" class="list-group-item">
+          {{ message.de }}: {{ message.cuerpo }}
+        </li>
+      </ul>
+    </div>
+
     <form @submit.prevent="submit">
       <input
         autofocus
         autocomplete="off"
         name="text"
-        class="form-control"
+        class="form-control mt-4"
         type="text"
         v-model="text"
       />
@@ -18,6 +26,7 @@
 
 <script>
 import chatService from '@/services/chatService';
+import Vue from 'vue';
 
 export default {
   name: 'Chat',
@@ -25,15 +34,23 @@ export default {
     return {
       text: '',
       chatService: new chatService(),
+      messages: []
     };
   },
   created() {
     this.chatService.getMessages((message) => {
       console.log({ message });
+      this.messages.push(message);
+      Vue.nextTick(() => {
+        this.$refs.chatMessage.scrollTop = this.$refs.chatMessage.scrollHeight;
+      });
     })
   },
   methods: {
     submit() {
+      if (!this.text.trim()) {
+        return;
+      }
       this.chatService.sendMessage(this.text);
       this.text = '';
     }
@@ -42,4 +59,11 @@ export default {
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped lang="scss"></style>
+<style scoped lang="scss">
+#chat-messages {
+  height: 300px;
+  border: 1px solid gray;
+  border-radius: 5px;
+  overflow-y: auto;
+}
+</style>
